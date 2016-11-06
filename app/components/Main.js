250c997968fcbcfe0f121f64bad2963e5338c576
Developer5262 <dylan.fernandes00@gmail.com>
2016-11-06 09:55:39 -0500

    added main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,27 @@
 var React = require('react');
 
+var styles = {
+    header: {
+        marginTop: 20,
+        color: 'lightgreen',
+        textAlign: 'center',
+        fontSize: '6rem'
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0
+    }
+}
+
 var Main = React.createClass({
     render: function() {
         return (
-            <div>
-                <h1>ToDo List</h1>
+            <div style = {styles.header_container}>
+                <h1 style = {styles.header}>Todo List</h1>
                 {this.props.children}
+                <div style = {styles.footer}>
+                    &copy; All rights reserved. Property of the CT Squad.
+                </div>
             </div>
         )
     }
